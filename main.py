5432e10edd24b1b104b17b91eafa38e974c63807
IvyWoof <al.hembrow@gmail.com>
2022-11-04 00:54:09 +0000

    Added database connection to store similarities

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import csv
-
-import pandas as pd
 from bs4 import BeautifulSoup
+import pandas as pd
+from sqlalchemy import create_engine
 from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 
 
-def read_xml():
-
+def read_mimosa_xml(engine):
+    i = 1
     name_list = []
     description_list = []
+    relationship_list = []
+    id_list = []
 
     # Opens mimosa xsd file
     with open("standards_files/mimosa_standards.xsd") as fp:
@@ -33,14 +33,29 @@ def read_xml():
             else:
                 description_list.append('N/A')
 
-    # Write name and description to csv file
-    csv_list = {'name': name_list, 'description': description_list}
+    for stored_name in name_list:
+        list_length = len(name_list)
+        for complex_type in soup.find_all('xs:complexType', {'name': stored_name}):
+            headers = [tag['type'] for tag in complex_type.find_all("xs:element", {'type': True})]
+            relationship_list.append(headers)
+
+        while i <= list_length:
+            id_list.append(i)
+            i += 1
+
+
+    # Write name and description to df
+    csv_list = {'idMimosa': id_list, 'name': name_list, 'description': description_list, 'relationships': 'test'}
     mimosa_df = pd.DataFrame(csv_list)
+    mimosa_df.to_csv('Data/test_concepts.csv', sep='\t')
+
+    # mimosa_df.to_sql('mimosa', con=engine, if_exists='append', chunksize=1000, index=False)
     return mimosa_df
 
 
-def read_plcs_xml():
-
+def read_plcs_xml(engine):
+    i = 1
+    id_list = []
     name_list = []
     description_list = []
 
@@ -58,6 +73,7 @@ def read_plcs_xml():
 
     # Finds and appends concept description to list
     for stored_name in name_list:
+        list_length = len(name_list)
         for complex_type in soup.find_all('xsd:complexType', {'name': stored_name}):
             complex_description = complex_type.find('xsd:documentation')
             if complex_description is not None:
@@ -66,17 +82,36 @@ def read_plcs_xml():
             else:
                 description_list.append('N/A')
 
-    # Write name and description to dataframe
-    df_list = {'name': name_list, 'description': description_list}
+        while i <= list_length:
+            id_list.append(i)
+            i += 1
+
+
+    # Write name and description to df
+    df_list = {'idPLCS': id_list, 'name': name_list, 'description': description_list, 'relationships': 'test'}
     plcs_df = pd.DataFrame(df_list)
+    plcs_df.to_csv('Data/test_plcs.csv', sep='\t')
+
+    # plcs_df.to_sql('plcs', con=engine, if_exists='append', chunksize=1000, index=False)
     return plcs_df
 
 
-def matches(dfm, dfp):
-    threshold = 70
+def connect_to_db():
+    # create sqlalchemy engine
+    engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                           .format(user="root",
+                                   pw="alanna1",
+                                   db="automatedmatching"))
+    return engine
+
+
+def name_match(dfm, dfp):
+    threshold = 0
     mlist = []
     plist = []
     slist = []
+    id_list = []
+    i = 0
 
     test = dfp['name'].values.tolist()
     test2 = dfm['name'].values.tolist()
@@ -87,37 +122,33 @@ def matches(dfm, dfp):
                 mlist.append(example)
                 plist.append(potential)
                 slist.append(similarity)
-
-    df_list = {'mimosa_name': mlist, 'plcs_name': plist, 'similarity': slist}
-    df = pd.DataFrame(df_list)
-    df.to_csv("output.csv", index=False)
-    return df
+                i += 1
+                id_list.append(i)
 
 
-def readCsv():
-    mimosa_name = 1
+    df_list = {'idSIM': id_list, 'id_plcs': plist, 'id_mimosa': mlist, 'sim_name': slist}
+    sim_df = pd.DataFrame(df_list)
+    return sim_df
 
-    prompt = "Enter a concept"
-    found_name = False
+def description_matching(dfm, dfp, df):
+    s_description = []
 
-    with open('output.csv', 'rt') as file:
-        reader = csv.reader(file)
-        next(reader, None)
-        name = input(prompt)
-        while not found_name:
-            for row in reader:
-                if name == row[mimosa_name]:
-                    found_name = True
-                    print(row)
-                    break
+    test = dfp['description'].values.tolist()
+    test2 = dfm['description'].values.tolist()
+    for potential in test:
+        for example in test2:
+            if example == 'N/A':
+                s_description.append('0')
             else:
-                name = input(f"Concept not in file. {prompt}")
-                file.seek(0)
+                similarity = fuzz.ratio(example, potential)
+                s_description.append(similarity)
 
+    df2 = df.assign(sim_description=s_description)
+    df2.to_sql('similarity', con=engine, if_exists='append', chunksize=1000, index=False)
+    return df2
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    mimosa_df = read_xml()
-    plcs_df = read_plcs_xml()
-    df = matches(mimosa_df, plcs_df)
-    readCsv()
+engine = connect_to_db()
+mimosa_df = read_mimosa_xml(engine)
+plcs_df = read_plcs_xml(engine)
+sim_df = name_match(mimosa_df, plcs_df)
+description_matching(mimosa_df, plcs_df, sim_df)
